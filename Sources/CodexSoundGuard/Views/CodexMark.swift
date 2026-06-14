52c589b3ced69b861e591231c66fee68ee6b98f4
whitewooood <975177209@qq.com>
2026-06-15 03:36:15 +0800

    Simplify menu bar usage icon

--- Sources/CodexSoundGuard/Views/CodexMark.swift
+++ Sources/CodexSoundGuard/Views/CodexMark.swift
@@ -71,37 +71,33 @@ struct CodexUsageMeter: View {
         image.lockFocus()
         defer { image.unlockFocus() }
 
-        let bodyRect = CGRect(x: 0.75, y: 1.25, width: 16, height: 15.5)
-        let bodyPath = NSBezierPath(roundedRect: bodyRect, xRadius: 4.5, yRadius: 4.5)
-        NSColor.labelColor.withAlphaComponent(0.08).setFill()
-        bodyPath.fill()
-        NSColor.labelColor.withAlphaComponent(0.58).setStroke()
-        bodyPath.lineWidth = 1
-        bodyPath.stroke()
+        drawStatusDot(in: CGRect(x: 14.1, y: 1.45, width: 4.8, height: 4.8))
 
         drawBar(
-            rect: CGRect(x: 3.5, y: 10.3, width: 10, height: 3),
+            rect: CGRect(x: 2.0, y: 10.7, width: 13.2, height: 3.2),
             usedPercent: usage?.primaryRateLimit?.usedPercent
         )
         drawBar(
-            rect: CGRect(x: 3.5, y: 5.0, width: 10, height: 3),
+            rect: CGRect(x: 2.0, y: 4.7, width: 13.2, height: 3.2),
             usedPercent: usage?.secondaryRateLimit?.usedPercent
         )
-
-        let dotPath = NSBezierPath(ovalIn: CGRect(x: 13.25, y: 0.9, width: 5.5, height: 5.5))
-        NSColor.windowBackgroundColor.withAlphaComponent(0.85).setFill()
-        dotPath.fill()
-
-        let innerDotPath = NSBezierPath(ovalIn: CGRect(x: 14.0, y: 1.65, width: 4, height: 4))
-        NSColor.labelColor.withAlphaComponent(statusIntensity).setFill()
-        innerDotPath.fill()
 
         return image
     }
 
+    private func drawStatusDot(in rect: CGRect) {
+        let haloPath = NSBezierPath(ovalIn: rect.insetBy(dx: -0.9, dy: -0.9))
+        NSColor.windowBackgroundColor.withAlphaComponent(0.82).setFill()
+        haloPath.fill()
+
+        let dotPath = NSBezierPath(ovalIn: rect)
+        NSColor.labelColor.withAlphaComponent(statusIntensity).setFill()
+        dotPath.fill()
+    }
+
     private func drawBar(rect: CGRect, usedPercent: Double?) {
         let trackPath = NSBezierPath(roundedRect: rect, xRadius: rect.height / 2, yRadius: rect.height / 2)
-        NSColor.labelColor.withAlphaComponent(0.18).setFill()
+        NSColor.labelColor.withAlphaComponent(0.16).setFill()
         trackPath.fill()
 
         guard let usedPercent else {
@@ -116,7 +112,7 @@ struct CodexUsageMeter: View {
         let fillWidth = max(1.2, rect.width * remainingProgress)
         let fillRect = CGRect(x: rect.minX, y: rect.minY, width: fillWidth, height: rect.height)
         let fillPath = NSBezierPath(roundedRect: fillRect, xRadius: rect.height / 2, yRadius: rect.height / 2)
-        NSColor.labelColor.withAlphaComponent(0.82).setFill()
+        NSColor.labelColor.withAlphaComponent(0.86).setFill()
         fillPath.fill()
     }
 }
